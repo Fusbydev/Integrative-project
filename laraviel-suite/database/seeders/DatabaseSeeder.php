2b71bad2c2f7451f5b7f0491dab7b6c1d1649852
Fabon, Kirk Alou John <114301753+Fusbydev@users.noreply.github.com>
2024-11-28 17:16:15 +0800

    Revert "committing middleware and breeze"

--- laraviel-suite/database/seeders/DatabaseSeeder.php
+++ laraviel-suite/database/seeders/DatabaseSeeder.php
@@ -5,8 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,20 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        DB::table('users')->insert([
-            'name'=> 'Admin',
-            'username'=> 'admin',
-            'email'=> 'admin@example.com',
-            'password' => Hash::make('password!'),
-            'role' => 'admin',
-        ]);
-
-        DB::table('users')->insert([
-            'name'=> 'Cashier',
-            'username'=> 'cashier',
-            'email'=> 'cashier@example.com',
-            'password' => Hash::make('password!'),
-            'role' => 'cashier',
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
